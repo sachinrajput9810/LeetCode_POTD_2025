53941f26499f8d39fe568d0c78e43a2d34546264
sachinrajput9810 <rajputsachin9810@gmail.com>
2025-01-01 21:37:54 +0530

    max score after splitting string

---
+++ Leetcode_Jan/day1_maximumScore.java
@@ -0,0 +1,23 @@
+package Leetcode_Jan;
+
+public class day1_maximumScore {
+    public int maxScore(String s) {
+        int n = s.length() ;
+        int one = 0 ;
+        int zero = 0 ;
+        int totalOne = 0 ;
+        int res = -1 ;
+        for(char ch : s.toCharArray()){
+            if(ch == '1') totalOne++ ;
+        }
+
+        for(int i=0 ; i<n-1 ; i++){
+            if(s.charAt(i) == '0') zero++ ;
+            else one++ ;
+
+            int score = zero + (totalOne - one) ;
+            res = Math.max(res , score) ;
+        }
+        return res ;
+    }
+}
