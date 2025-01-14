76b638861b7a490267b821b25d1668ab456b1e93
sachinrajput9810 <rajputsachin9810@gmail.com>
2025-01-14 20:57:14 +0530

    Find prefix common array

---
+++ Leetcode_Jan/day_14_prefix_common_array.java
@@ -0,0 +1,21 @@
+package Leetcode_Jan;
+
+import java.util.HashSet;
+
+public class day_14_prefix_common_array{
+     public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length ; 
+        int res[] = new int[n] ;
+        HashSet<Integer> set = new HashSet<>() ;
+        for(int i=0 ; i<n ; i++){
+            int val1 = A[i] ;
+            int val2 = B[i] ;
+            set.add(val1) ;
+            set.add(val2) ;
+            int setSize = set.size() ;
+            int commonEle = (i+1)*2 - setSize ;
+            res[i] = commonEle ;
+        }
+        return res ;
+    }
+}
